f468ed54b5767ab783fb1b6593c3374e4496fdae
Minjun Kim <minjun4799@gmail.com>
2024-09-26 19:06:05 +0900

    pp9 done. adding digits in to morse_table (binary tree)

--- pp9/morse.py
+++ pp9/morse.py
@@ -12,12 +12,17 @@ class Morse():
 
   def decode(self, morse):
     if morse == "":
+      # 해석할 기호가 남아있지 않을시, 해당 시점의 노드에 저장된 symbol을 변환함.
       return self.symbol
     elif morse[0] == '.':
+      # 만약 morse 문자열의 첫번째 기호가 '.'이면 self.dot을 참조하여 왼쪽 자식으로 향함.
       if self.dot is None:
+        # 왼쪽 자식이 없다면 None을 반환.
         return None
+      # 정상적으로 존재 한다면, 두번째 부호부터 다시 재귀적으로 반복함.
       return self.dot.decode(morse[1:])
     elif morse[0] == '-':
+      # 마찬가지로 morse 문자열의 첫번째 기호가 '-'이면 self.dash를 참조하여 오른쪽 자식으로 향하고, 두번째 부호부터 재귀적으로 반복.
       if self.dash is None:
         return None
       return self.dash.decode(morse[1:])
@@ -26,6 +31,14 @@ class Morse():
     raise NotImplementedError
 
 # The decoding tree
+# 0~9까지의 digit을 추가해야함.
+# 1: ".----" 2: "..---" 3: "...--" 4: "....-" 5: "....."
+# 6: "-...." 7: "--..." 8: "---.." 9: "----." 0: "-----"
+
+# 다만, 위를 추가하기전에 해당 공간에 들어갈 '빈 노드'를 먼저 생성해줘야 한다.
+# if morse_table.dot.dash.dash.dash is None:
+#     morse_table.dot.dash.dash.dash = Morse()
+# morse_table.dot.dash.dash.dash.dash = Morse('1')
 
 morse_table = Morse(None, 
                     Morse('E', 
@@ -57,6 +70,52 @@ morse_table = Morse(None,
                                       Morse('Q')),
                                 Morse('O'))))
 
+# 숫자 1 추가
+if morse_table.dot.dash.dash.dash is None:
+    morse_table.dot.dash.dash.dash = Morse()
+morse_table.dot.dash.dash.dash.dash = Morse('1')
+
+# 숫자 2 추가
+if morse_table.dot.dot.dash.dash is None:
+    morse_table.dot.dot.dash.dash = Morse()
+morse_table.dot.dot.dash.dash.dash = Morse('2')
+
+# 숫자 3 추가
+if morse_table.dot.dot.dot.dash is None:
+    morse_table.dot.dot.dot.dash = Morse()
+morse_table.dot.dot.dot.dash.dash = Morse('3')
+
+# 숫자 4 추가
+if morse_table.dot.dot.dot.dot is None:
+    morse_table.dot.dot.dot.dot = Morse()
+morse_table.dot.dot.dot.dot.dash = Morse('4')
+
+# 숫자 5 추가
+if morse_table.dot.dot.dot.dot.dot is None:
+    morse_table.dot.dot.dot.dot.dot = Morse('5')
+
+# 숫자 6 추가
+if morse_table.dash.dot.dot.dot is None:
+    morse_table.dash.dot.dot.dot = Morse()
+morse_table.dash.dot.dot.dot.dot = Morse('6')
+
+# 숫자 7 추가
+if morse_table.dash.dash.dot.dot is None:
+    morse_table.dash.dash.dot.dot = Morse()
+morse_table.dash.dash.dot.dot.dot = Morse('7')
+
+# 숫자 8 추가
+if morse_table.dash.dash.dash.dot is None:
+    morse_table.dash.dash.dash.dot = Morse()
+morse_table.dash.dash.dash.dot.dot = Morse('8')
+
+# 숫자 9 추가
+if morse_table.dash.dash.dash.dash is None:
+    morse_table.dash.dash.dash.dash = Morse()
+morse_table.dash.dash.dash.dash.dot = Morse('9')
+
+# 숫자 0 추가
+morse_table.dash.dash.dash.dash.dash = Morse('0')
 # --------------------------------------------------------------------
 
 def decode_transmission(s):
